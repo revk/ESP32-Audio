a2528ff0bb6f6afb561f1365e2d9af92a6d9be02
RevK <github@k.gg>
2024-11-23 10:05:22 +0000

    SD mount

--- main/Audio.c
+++ main/Audio.c
@@ -10,6 +10,7 @@ static const char TAG[] = "Audio";
 #include <driver/uart.h>
 #include <driver/i2c.h>
 #include <driver/i2s_std.h>
+#include <driver/i2s_pdm.h>
 #include <esp_http_server.h>
 #include "fft.h"
 #include "math.h"
@@ -67,7 +68,18 @@ char *dtmfmessage = NULL;       // Malloc'd
 
 struct
 {
+   uint8_t die:1;               // Shutting down
+   uint8_t sdpresent:1;         // SD present
+   uint8_t doformat:1;          // SD format
+   uint8_t dodismount:1;        // Dismount SD
+   uint8_t micok:1;             // We see sound
+   uint8_t micon:1;             // Sounds required
 } b;
+const char sd_mount[] = "/sd";
+char rgbsd = 0;                 // Colour for SD card
+const char *cardstatus = NULL;  // Status of SD card
+uint64_t sdsize = 0,            // SD card data
+   sdfree = 0;
 
 static httpd_handle_t webserver = NULL;
 
@@ -137,6 +149,244 @@ web_root (httpd_req_t * req)
 SemaphoreHandle_t audio_mutex = NULL;
 
 void
+sd_task (void *arg)
+{
+   esp_err_t e = 0;
+   revk_gpio_input (sdcd);
+   // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+   // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+   // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+   sdmmc_host_t host = SDSPI_HOST_DEFAULT ();
+   host.max_freq_khz = SDMMC_FREQ_PROBING;
+   spi_bus_config_t bus_cfg = {
+      .mosi_io_num = sdmosi.num,
+      .miso_io_num = sdmiso.num,
+      .sclk_io_num = sdsck.num,
+      .quadwp_io_num = -1,
+      .quadhd_io_num = -1,
+      .max_transfer_sz = 4000,
+   };
+   e = spi_bus_initialize (host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+   if (e != ESP_OK)
+   {
+      rgbsd = 'R';
+      jo_t j = jo_object_alloc ();
+      jo_string (j, "error", cardstatus = "SPI failed");
+      jo_int (j, "code", e);
+      jo_int (j, "MOSI", sdmosi.num);
+      jo_int (j, "MISO", sdmiso.num);
+      jo_int (j, "CLK", sdsck.num);
+      revk_error ("SD", &j);
+      vTaskDelete (NULL);
+      return;
+   }
+   esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+      .format_if_mount_failed = 1,
+      .max_files = 2,
+      .allocation_unit_size = 16 * 1024,
+      .disk_status_check_enable = 1,
+   };
+   sdmmc_card_t *card = NULL;
+   sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT ();
+   //slot_config.gpio_cs = sdss.num;
+   slot_config.gpio_cs = -1;
+   revk_gpio_output (sdss, 0);  // Bodge for faster SD card access in ESP IDF V5+
+   slot_config.host_id = host.slot;
+   while (!b.die)
+   {
+      if (sdcd.set)
+      {
+         if (b.dodismount)
+         {                      // Waiting card removed
+            rgbsd = 'B';
+            jo_t j = jo_object_alloc ();
+            jo_string (j, "action", cardstatus = revk_shutting_down (NULL) ? "Card dismounted for shutdown" : "Remove card");
+            revk_info ("SD", &j);
+            b.dodismount = 0;
+            while ((b.sdpresent = revk_gpio_get (sdcd)))
+               sleep (1);
+            continue;
+         }
+         if (!(b.sdpresent = revk_gpio_get (sdcd)))
+         {                      // No card
+            jo_t j = jo_object_alloc ();
+            jo_string (j, "error", cardstatus = "Card not present");
+            revk_info ("SD", &j);
+            rgbsd = 'M';
+            revk_enable_wifi ();
+            revk_enable_ap ();
+            while (!(b.sdpresent = revk_gpio_get (sdcd)))
+               sleep (1);
+         }
+         if (!wifidebug)
+            revk_disable_wifi ();
+         revk_disable_ap ();
+         b.sdpresent = 1;
+      } else if (b.dodismount)
+      {
+         b.dodismount = 0;
+         sleep (60);
+         continue;
+      }
+      sleep (1);
+      ESP_LOGI (TAG, "Mounting filesystem");
+      e = esp_vfs_fat_sdspi_mount (sd_mount, &host, &slot_config, &mount_config, &card);
+      if (e != ESP_OK)
+      {
+         jo_t j = jo_object_alloc ();
+         if (e == ESP_FAIL)
+            jo_string (j, "error", cardstatus = "Failed to mount");
+         else
+            jo_string (j, "error", cardstatus = "Failed to iniitialise");
+         jo_int (j, "code", e);
+         revk_error ("SD", &j);
+         rgbsd = 'R';
+         sleep (1);
+         continue;
+      }
+      ESP_LOGI (TAG, "Filesystem mounted");
+      b.sdpresent = 1;          // we mounted, so must be
+      rgbsd = 'G';              // Writing to card
+      if (b.doformat && (e = esp_vfs_fat_spiflash_format_rw_wl (sd_mount, "Audio")))
+      {
+         jo_t j = jo_object_alloc ();
+         jo_string (j, "error", cardstatus = "Failed to format");
+         jo_int (j, "code", e);
+         revk_error ("SD", &j);
+      }
+      rgbsd = 'R';              // Oddly this call can hang forever!
+      {
+         esp_vfs_fat_info (sd_mount, &sdsize, &sdfree);
+         jo_t j = jo_object_alloc ();
+         jo_string (j, "action", cardstatus = (b.doformat ? "Formatted" : "Mounted"));
+         jo_int (j, "size", sdsize);
+         jo_int (j, "free", sdfree);
+         revk_info ("SD", &j);
+      }
+      rgbsd = 'Y';              // Mounted, ready
+      b.doformat = 0;
+      while (!b.doformat && !b.dodismount && !b.die)
+      {
+         FILE *o = NULL;
+         char filename[100];
+         while (!b.doformat && !b.dodismount)
+         {
+            rgbsd = (o ? 'G' : 'Y');
+            if (!(b.sdpresent = revk_gpio_get (sdcd)))
+            {                   // card removed
+               b.dodismount = 1;
+               break;
+            }
+            // TODO open file and write data
+            sleep (1);
+         }
+         rgbsd = 'B';
+         // All done, unmount partition and disable SPI peripheral
+         esp_vfs_fat_sdcard_unmount (sd_mount, card);
+         ESP_LOGI (TAG, "Card dismounted");
+         {
+            jo_t j = jo_object_alloc ();
+            jo_string (j, "action", cardstatus = "Dismounted");
+            revk_info ("SD", &j);
+         }
+      }
+   }
+   vTaskDelete (NULL);
+}
+
+void
+mic_task (void *arg)
+{
+   jo_t e (esp_err_t err, const char *msg)
+   {                            // Error
+      jo_t j = jo_object_alloc ();
+      if (msg)
+         jo_string (j, "message", msg);
+      if (err)
+         jo_string (j, "error", esp_err_to_name (err));
+      if (micdata.set)
+         jo_int (j, "data", micdata.num);
+      if (micclock.set)
+         jo_int (j, "clock", micclock.num);
+      return j;
+   }
+   esp_err_t err;
+   i2s_chan_handle_t i = { 0 };
+   i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG (I2S_NUM_AUTO, I2S_ROLE_MASTER);
+   err = i2s_new_channel (&chan_cfg, NULL, &i);
+   uint32_t samples = micrate / 50;
+   uint8_t bytes = (micws.set ? 8 : 4); // No WS means PDM (16 bit)
+   uint8_t *micraw = mallocspi (bytes * samples);
+   if (micws.set)
+   {                            // 24 bit Philips format
+      ESP_LOGE (TAG, "Mic init CLK %d DAT %d WS %d", micclock.num, micdata.num, micws.num);
+      i2s_std_config_t cfg = {
+         .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG (micrate),
+         .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG (I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
+         .gpio_cfg = {
+                      .mclk = I2S_GPIO_UNUSED,
+                      .bclk = micclock.num,
+                      .ws = micws.num,
+                      .dout = I2S_GPIO_UNUSED,
+                      .din = micdata.num,
+                      .invert_flags = {
+                                       .mclk_inv = false,
+                                       .bclk_inv = micclock.invert,
+                                       .ws_inv = micws.invert,
+                                       },
+                      },
+      };
+      cfg.slot_cfg.slot_mask = I2S_STD_SLOT_BOTH;
+      if (bytes == 3)
+         cfg.clk_cfg.mclk_multiple = 384;
+      if (!err)
+         err = i2s_channel_init_std_mode (i, &cfg);
+   } else
+   {                            // PDM 16 bit
+      ESP_LOGE (TAG, "Mic init PDM CLK %d DAT %d", micclock.num, micdata.num);
+      i2s_pdm_rx_config_t cfg = {
+         .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG (micrate),
+         .slot_cfg = I2S_PDM_RX_SLOT_DEFAULT_CONFIG (I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
+         .gpio_cfg = {
+                      .clk = micclock.num,
+                      .din = micdata.num,
+                      .invert_flags = {
+                                       .clk_inv = micclock.invert}
+                      }
+      };
+      cfg.slot_cfg.slot_mask = I2S_PDM_SLOT_BOTH;
+      if (!err)
+         err = i2s_channel_init_pdm_rx_mode (i, &cfg);
+   }
+   gpio_pulldown_en (micdata.num);
+   if (!err)
+      err = i2s_channel_enable (i);
+   if (err)
+   {
+      ESP_LOGE (TAG, "Mic I2S failed");
+      jo_t j = e (err, "Failed init I2S");
+      revk_error ("i2s", &j);
+      vTaskDelete (NULL);
+      return;
+   }
+   ESP_LOGE (TAG, "Mic started, %ld*%d bits at %ldHz", samples, bytes * 8, micrate);
+   while (1)
+   {
+      size_t n = 0;
+      i2s_channel_read (i, micraw, bytes * samples, &n, 100);
+      if (n < bytes * samples)
+         continue;
+      if (*(int32_t *) micraw)
+         b.micok = 1;
+      if (!b.micon)
+         continue;              // Not needed
+      // TODO
+
+   }
+   vTaskDelete (NULL);
+}
+
+void
 spk_task (void *arg)
 {
    esp_err_t e = 0;
@@ -169,7 +419,7 @@ spk_task (void *arg)
       e = i2s_channel_enable (tx_handle);
    if (e)
    {
-      ESP_LOGE (TAG, "I2S failed");
+      ESP_LOGE (TAG, "Spk I2S failed");
       jo_t j = jo_object_alloc ();
       jo_string (j, "error", esp_err_to_name (e));
       revk_error ("spk", &j);
@@ -337,6 +587,10 @@ app_main ()
 
    if (spklrc.set && spkbclk.set && spkdata.set)
       revk_task ("spk", spk_task, NULL, 8);
+   if (micdata.set && micclock.set)
+      revk_task ("mic", mic_task, NULL, 8);
+   if (sdss.set && sdmosi.set && sdmiso.set && sdsck.set)
+      revk_task ("sd", sd_task, NULL, 8);
 
    httpd_config_t config = HTTPD_DEFAULT_CONFIG ();     // When updating the code below, make sure this is enough
    //  Note that we 're also 4 adding revk' s web config handlers
@@ -345,5 +599,12 @@ app_main ()
    {
       revk_web_settings_add (webserver);
       register_get_uri ("/", web_root);
+   }
+   // Buttons and LEDs
+   while (1)
+   {
+      usleep (100000);
+      // TODO
+
    }
 }
